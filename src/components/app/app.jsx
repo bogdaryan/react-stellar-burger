b045f36e3397f07c8bc8d26c11b06126b4287c4c
Bogdan <bogdan10050@gmail.com>
2023-10-18 20:21:24 +0300

    feat: add file structure

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,12 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import Header from "../app-header/";
 
 function App() {
   return (
-    <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
-    </div>
+    <>
+      <div></div>
+    </>
   );
 }
 
---
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BurgerConstructor(props) {
+  return <div></div>;
+}
+
+export default BurgerConstructor;
